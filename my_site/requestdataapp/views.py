37e77ff39cb7bca69ad3b98e103a62a02a06a0e1
Andrey Ivanov <andr.ivanov-ptz@yandex.ru>
2023-05-14 16:08:51 +0300

    Выполнил практическую работу №6 по Django

--- my_site/requestdataapp/views.py
+++ my_site/requestdataapp/views.py
@@ -1,6 +1,8 @@
 from django.core.files.storage import FileSystemStorage
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from .forms import UserBioForm, UploadFileForm
+
 
 def process_get_view(request: HttpRequest) -> HttpResponse:
     a = request.GET.get("a", "")
@@ -15,17 +17,29 @@ def process_get_view(request: HttpRequest) -> HttpResponse:
 
 
 def user_form(request: HttpRequest) -> HttpResponse:
-    return render(request, 'requestdataapp/user-bio-form.html')
+    context = {
+        "form":UserBioForm(),
+    }
+    return render(request, 'requestdataapp/user-bio-form.html', context=context)
 
 def handle_file_upload(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST" and request.FILES.get("myfile"):
-        myfile = request.FILES["myfile"]
-        fs = FileSystemStorage()
-        if fs.size(myfile.name) < 10:
-            filename = fs.save(myfile.name, myfile)
-            print("saved file", filename)
-        else:
-            print("File not saved")
-            return render(request, 'requestdataapp/err_files.html')
+    if request.method == "POST":
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            # myfile = request.FILES["myfile"]
+            myfile = form.cleaned_data["file"]
+            if myfile.size < 1024*1024:
+                fs = FileSystemStorage()
+                filename = fs.save(myfile.name, myfile)
+                print("saved file", filename)
+            else:
+                print("File not saved")
+                return render(request, 'requestdataapp/err_files.html')
+    else:
+        form = UploadFileForm()
 
-    return render(request, 'requestdataapp/file-upload.html')
+    context = {
+        "form": form,
+    }
+
+    return render(request, 'requestdataapp/file-upload.html', context=context)
