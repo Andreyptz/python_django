284fec27aed3da143ea353d9d92008a628d7d5a0
Andrey Ivanov <andr.ivanov-ptz@yandex.ru>
2023-08-28 08:25:09 +0300

    Практическая работа 16 готова

--- my_site/blogapp/models.py
+++ my_site/blogapp/models.py
@@ -8,6 +8,8 @@ class Author(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=40, db_index=True)
+    def __str__(self):
+        return self.name
 
 class Tag(models.Model):
     name = models.CharField(max_length=20, db_index=True)
@@ -20,6 +22,5 @@ class Article(models.Model):
     content = models.TextField(null=False, blank=True, db_index=True)
     pub_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     tags = models.ManyToManyField(Tag, related_name="articles")
-    def __str__(self):
-        return self.title
